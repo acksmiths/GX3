fadc2f59a814c255e8bebe685b03052b118ece04
Senna <miagrasialeano@gmail.com>
2023-05-24 11:09:20 +0800

    Initial commit

---
+++ Smart/sm.js
@@ -0,0 +1,21 @@
+// Add JavaScript to show and hide content based on button clicks
+//var wc = "Welcome to my homepage!";
+//var maaps = "THIS IS THE MAP";
+//var menu = "This is the menu";
+//var about = "About me: I am a web developer who loves creating interactive web experiences.";
+var BC = document.getElementById("BAGUIO");
+var SB = document.getElementById("SABLAN");
+var TB = document.getElementById("TUBA");
+
+
+
+
+var BC = function (){
+    document.getElementById("content").innerHTML = BC;
+};
+var SB = function (){
+    document.getElementById("content").innerHTML = SB;
+};
+var TB = function (){
+    document.getElementById("content").innerHTML = TB;
+};
